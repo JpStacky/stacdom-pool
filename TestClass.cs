8fbf4f3a7a2ea9a1b8592fc63cf3ac1147deb919
Jp <jyotiprakashpc@outlook.com>
2022-09-06 22:53:34 +0530

    Update column

---
+++ TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONet
+{
+    public class TestClass
+    {
+        public static void Main()
+        {
+            UpdateData updateData = new UpdateData();
+            updateData.Update();
+        }
+    }
+}
---
+++ UpdateData.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+namespace ADONet
+{
+    public class UpdateData
+    {
+        public void Update()
+        {
+            try
+            {
+                string connstring = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = payroll_service; Integrated Security = true";
+                SqlConnection con = new SqlConnection(connstring);
+                con.Open();
+                int sal = 3000;
+                string name = "TERISA";
+                string query = "UPDATE EMPLOYEE_PAYROLL SET SALARY = "+sal+" WHERE NAME = "+name;
+                SqlCommand cmd = new SqlCommand(query, con);
+               cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
